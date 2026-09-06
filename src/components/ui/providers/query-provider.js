40e7e33c362e58754275f79a05f38f30c27d05ff
Bittu Kumar <bk7355583@gmail.com>
2026-09-06 14:52:55 +0530

    global providers

---
+++ src/components/ui/providers/query-provider.js
@@ -0,0 +1,26 @@
+"use client"
+
+import * as React from "react"
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query"
+
+export function QueryProvider({ children }) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 30 * 1000, // 30 seconds
+          },
+        },
+      })
+  )
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
